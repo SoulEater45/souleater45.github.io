9ce03fdb0ed27b9e581f8f360d953b407b38d5e5
Mirza <mirza.arnaut@tu-dortmund.de>
2021-08-21 04:49:39 +0200

    Added a landing page

---
+++ portfolio-ng/firsttry/assets/js/load-rive.js
@@ -0,0 +1,7 @@
+function loadRive() {
+  new rive.Rive({
+      src: 'https://cdn.rive.app/animations/off_road_car_v7.riv',
+      canvas: document.getElementById('canvas'),
+      autoplay: true,
+    });
+}
