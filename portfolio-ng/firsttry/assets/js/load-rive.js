87a45693044e8374c793d7fcbab1a7df7adfaea4
Mirza <mirza.arnaut@tu-dortmund.de>
2021-08-23 03:40:11 +0200

    Added some color and dynamic (?) wavy dividers

--- portfolio-ng/firsttry/assets/js/load-rive.js
+++ portfolio-ng/firsttry/assets/js/load-rive.js
@@ -1,6 +1,7 @@
 function loadRive() {
   new rive.Rive({
-      src: 'https://cdn.rive.app/animations/off_road_car_v7.riv',
+      // src: 'https://cdn.rive.app/animations/off_road_car_v7.riv',
+      src: 'rive/earth.riv',
       canvas: document.getElementById('canvas'),
       autoplay: true,
     });
